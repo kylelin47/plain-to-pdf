968468a0204060bb57bd8366918670206bad0f42
Kyle Lin <kylelin47@gmail.com>
2015-07-26 08:28:45 -0400

    Logs are saved. Errors printed to stdout

--- plain_to_pdf.sh
+++ plain_to_pdf.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+move_logs()
+{
+    log_folder="$1"/logs
+    echo "Moving logs to ${log_folder}"
+    mkdir -p "$log_folder"
+    mv "$1"/*.log "$log_folder"
+}
 getopts ":o:" opt
 if [ -z "$OPTARG" ];
     then output=.;
@@ -8,3 +15,4 @@ mkdir -p "$output"
 for script in "`dirname $0`/scripts/convert_"*.sh; do
     "$script" "$output"
 done
+move_logs "$output"
--- scripts/convert_latex.sh
+++ scripts/convert_latex.sh
@@ -2,12 +2,11 @@
 clean()
 {
     rm "${1}.aux"
-    rm "${1}.log"
     rm "${1}.out"
 }
 find -type f -name "*.tex" | while read file; do
     output="$1/"$(basename "$file" .tex)
     echo "Converting file ${file} to ${output}.pdf"
-    pdflatex -interaction=batchmode -jobname="$output" "$file"
+    pdflatex -jobname="$output" -halt-on-error "$file" | grep -a3 ^!
     clean "$output"
 done
