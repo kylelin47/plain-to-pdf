bf80db92487d4f88e4256c24ef312a09566fedca
Kyle Lin <kylelin47@gmail.com>
2015-07-26 11:16:35 -0400

    Switch from pdflatex to xelatex

--- scripts/convert_latex.sh
+++ scripts/convert_latex.sh
@@ -7,6 +7,6 @@ clean()
 find -type f -name "*.tex" | while read file; do
     output="$1/"$(basename "$file" .tex)
     echo "Converting file ${file} to ${output}.pdf"
-    pdflatex -jobname="$output" -halt-on-error "$file" | grep -a3 ^!
+    xelatex -jobname="$output" -halt-on-error "$file" | grep -a3 ^!
     clean "$output"
 done
