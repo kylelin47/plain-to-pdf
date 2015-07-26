d9aab44fbc8502315a6ea8f7d8954092a9d30281
Kyle Lin <kylelin47@gmail.com>
2015-07-26 11:37:09 -0400

    Remove example text. Cleaner use of quotes in .sh

--- scripts/convert_latex.sh
+++ scripts/convert_latex.sh
@@ -5,7 +5,7 @@ clean()
     rm "${1}.out"
 }
 find -type f -name "*.tex" | while read file; do
-    output="$1/"$(basename "$file" .tex)
+    output="$1"/$(basename "$file" .tex)
     echo "Converting file ${file} to ${output}.pdf"
     xelatex -jobname="$output" -halt-on-error "$file" | grep -a3 ^!
     clean "$output"
