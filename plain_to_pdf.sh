d0c6bff9fde3df6403a6046f7c422c45a719742c
Kyle Lin <kylelin47@gmail.com>
2015-08-15 16:45:21 -0400

    Prettier output. Ignore *_structure.tex

--- plain_to_pdf.sh
+++ plain_to_pdf.sh
@@ -4,13 +4,24 @@ move_logs()
     log_folder="$1"/logs
     echo "Moving logs to ${log_folder}"
     mkdir -p "$log_folder"
-    mv "$1"/*.log "$log_folder"
+    mv "$1"/*.log "$log_folder" 2> /dev/null
 }
-getopts ":o:" opt
-if [ -z "$OPTARG" ];
-    then output=.;
-else output="$OPTARG";
-fi
+output=.
+while getopts ":o:" opt; do
+    case $opt in
+        o)
+          output="$OPTARG"
+          ;;
+        \?)
+          echo "Invalid option: -$OPTARG" >&2
+          exit 1
+          ;;
+        :)
+          echo "Option -$OPTARG requires an argument." >&2
+          exit 1
+          ;;
+    esac
+done
 mkdir -p "$output"
 for script in "`dirname $0`/scripts/convert_"*.sh; do
     "$script" "$output"
--- scripts/convert_latex.sh
+++ scripts/convert_latex.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 clean()
 {
-    rm "${1}.aux"
-    rm "${1}.out"
+    rm "${1}.aux" 2> /dev/null
+    rm "${1}.out" 2> /dev/null
 }
-find -type f -name "*.tex" | while read file; do
+find -type f -name "*.tex" ! -name "*_structure.tex" | while read file; do
     output="$1"/$(basename "$file" .tex)
     echo "Converting file ${file} to ${output}.pdf"
     xelatex -jobname="$output" -halt-on-error "$file" | grep -a3 ^!
